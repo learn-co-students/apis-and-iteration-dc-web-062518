3343022d1a1d95acbd9f8af2a0ef32352942b356
whynotski <laurawynosky@gmail.com>
2018-06-26 16:37:16 -0400

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,20 +6,49 @@ def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
+
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
+  results = character_hash["results"]
+  character_films = []
+  results.each do |person|
+    if person["name"] == character
+  
+      character_films << person["films"]
+    end
+  end
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
+  film_collection = []
+
+  character_films[0].each do |url|
+    film_info = RestClient.get(url)
+    film_info_hash = JSON.parse(film_info)
+    film_collection << film_info_hash
+  end
+
   # return value of this method should be collection of info about each film.
   #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
+  return film_collection
+
+
+end
+
+
+  #`parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
-end
+
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  num = 1
+  puts "This character appears in:"
+  films_hash.each do |info_log|
+    puts "#{num}. Episode #{info_log["episode_id"]} - #{info_log["title"]} - Directed by: #{info_log["director"]}"
+    num+=1
+  end
+
 end
 
 def show_character_movies(character)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,11 @@
 def welcome
   # puts out a welcome message here!
+  puts "Welcome to Star Wars!!  Who is your favorite character?"
 end
 
 def get_character_from_user
-  puts "please enter a character"
+  puts "Please enter your favorite character's name"
+  input = gets.strip
+  return input
   # use gets to capture the user's input. This method should return that input, downcased.
 end
