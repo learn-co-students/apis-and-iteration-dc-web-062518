482840d3d166c4059c1162d4e12ed3dcacc1d27e
Josh <joshua.ross.miller@gmail.com>
2018-06-26 15:53:16 -0400

    done with lab

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,7 +6,8 @@ def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
+  films_arr = []
+  # binding.pry
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -16,10 +17,37 @@ def get_character_movies_from_api(character)
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
+
+  character_hash["results"].each do |item|
+    if item['name'] == character
+      item.each do |key,value|
+        if key == "films"
+          value.each do |film|
+            sw_film = RestClient.get(film)
+            film_hash = JSON.parse(sw_film)
+            # film_hash = parse_character_movies(sw_film)
+            films_arr << film_hash["title"]
+          end
+        end
+      end
+    end
+  end
+  # binding.pry
+  films_arr.each_with_index do |film,index|
+    puts "#{index+1}. #{film}"
+  end
 end
+
+# def get_character(hash)
+#   name = hash["results"][0]["name"]
+#   binding.pry
+# end
+get_character_movies_from_api("Luke Skywalker")
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  movie_hash = JSON.parse(films_hash)
+  movie_hash
 end
 
 def show_character_movies(character)
@@ -27,7 +55,6 @@ def show_character_movies(character)
   parse_character_movies(films_hash)
 end
 
-## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
